27951b7fd02779638ed2bed15e0891bd23dd9a10
danigit <ds.acconto@gmail.com>
2018-12-25 04:00:50 +0100

    HOME/LOGIN PAGES
    Creata la pagina home con un pulsante che porta alla pagina di login, creata la pagina di login con due input. Entrambe sono senza styling

--- app/app.js
+++ app/app.js
@@ -1,9 +1,29 @@
-'use strict';
+(function () {
+    'use strict';
 
-let app = angular.module('myApp', []);
+    let app = angular.module('main', ['ngRoute']);
 
-app.controller('myCtrl', function ($scope) {
-    $scope.cars = [
-        'acura', 'audi', 'BMW'
-    ];
-});
+    app.config(function ($routeProvider) {
+        $routeProvider.when('/', {
+            templateUrl: './components/home.html',
+            controller: 'homeController'
+        }).when('/login', {
+            templateUrl: './components/login.html',
+            controller: 'loginController'
+        })
+        .otherwise({
+            template: '404'
+        })
+    });
+
+    app.controller('homeController', function ($scope, $location) {
+        $scope.login = function () {
+            let username = $scope.username;
+            let password = $scope.password;
+        }
+    });
+
+    app.controller('loginController', function ($scope) {
+
+    })
+})();
